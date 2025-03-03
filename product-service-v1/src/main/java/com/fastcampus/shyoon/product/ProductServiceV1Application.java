5fc0836df01431805b17702971a3e80164d996ed
shyoon <shyoon991@gmail.com>
2025-03-03 16:44:20 +0900

    v1: Initial Project

---
+++ product-service-v1/src/main/java/com/fastcampus/shyoon/product/ProductServiceV1Application.java
@@ -0,0 +1,13 @@
+package com.fastcampus.shyoon.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceV1Application.class, args);
+    }
+
+}
---
+++ product-service-v1/src/test/java/com/fastcampus/shyoon/product/ProductServiceV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.shyoon.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
