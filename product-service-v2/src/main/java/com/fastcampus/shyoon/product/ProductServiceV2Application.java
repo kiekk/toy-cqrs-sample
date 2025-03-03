60a8629db0f58fa6f83cb19f1405625e1852c135
shyoon <shyoon991@gmail.com>
2025-03-03 17:45:31 +0900

    v2: Initial Project

---
+++ product-service-v2/src/main/java/com/fastcampus/shyoon/product/ProductServiceV2Application.java
@@ -0,0 +1,13 @@
+package com.fastcampus.shyoon.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceV2Application.class, args);
+    }
+
+}
